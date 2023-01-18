ef47b0d0fbacf095a58e5925687de47be2f96bcb
Wrismynd <igorlutykk@gmail.com>
2023-01-18 23:59:49 +0200

    upd without thrt

---
+++ src/js/01-gallery.js
@@ -0,0 +1,33 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items';
+
+const galleryList = document.querySelector('.gallery');
+
+renderGalleryItems(galleryItems);
+const gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250});
+
+galleryList.addEventListener('click', onGalleryListClick)
+function onGalleryListClick(event) {
+    event.preventDefault();
+};
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                    />
+            </a>
+        </li>
+    `).join('')
+}
+
+
+function renderGalleryItems(galleryItems) {
+    galleryList.innerHTML = createGalleryItemsMarkup(galleryItems);
+}
